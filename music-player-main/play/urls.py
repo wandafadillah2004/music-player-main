7348a537b20b1d08660777da3c5ce09aee383331
WANDA FADILLAH <wanda2004.solo@gmail.com>
2022-07-18 16:10:59 +0700

    PROJECT UAS

---
+++ music-player-main/play/urls.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from . import views
+app_name = "play"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
